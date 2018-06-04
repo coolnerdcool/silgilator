56ce5086524aa77212244cbc20b7e2a5a3c7245c
Michel Ortega <coolnerdcool@gmail.com>
2018-06-04 15:38:29 -0500

    [Feat] Sigilator inits.

---
+++ silgilator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  silgilator
+//
+//  Created by Michel Ortega on 6/4/18.
+//  Copyright © 2018 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
