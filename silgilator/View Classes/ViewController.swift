a97a66d137a0abb9769aa7b65554ded68c9924b1
Michel Ortega <coolnerdcool@gmail.com>
2018-06-05 15:47:11 -0500

    [FEAT] Add two text labels in storyboard.

---
+++ silgilator/View Classes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  silgilator
+//
+//  Created by Michel Ortega on 6/4/18.
+//  Copyright © 2018 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+  override func viewWillAppear(_ animated: Bool) {
+    
+    
+    
+  }
+  
+  override func viewDidLoad() {
+    
+  }
+  
+}
+
