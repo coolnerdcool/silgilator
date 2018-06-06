6cf495bf0c00d1a9447baa1789fecc3e145fdc27
Michel Ortega <coolnerdcool@gmail.com>
2018-06-06 15:13:40 -0500

    [FEAT] Implemented current date in text label.

--- silgilator/View Classes/ViewController.swift
+++ silgilator/View Classes/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
  
+  @IBOutlet var dateLabel: UILabel!
+  
+  let year = Calendar.current.component(.year, from: Date())
+  let month = Calendar.current.component(.month, from: Date())
+  let date = Calendar.current.component(.day, from: Date())
+//
+  
   override func viewWillAppear(_ animated: Bool) {
-    
-    
     
   }
   
   override func viewDidLoad() {
+    
+    dateLabel.text = "\(Calendar.current.shortMonthSymbols[month - 1]) \(date) \(year)"
     
   }
   
